f0b362c42e600c4795ad02966a067576edc41655
Rob <rob_zoszak@yahoo.com.au>
2017-11-22 19:19:03 +0800

    refactor for style

--- index.js
+++ index.js
@@ -4,32 +4,21 @@ const swaggerUiAssetPath = require('swagger-ui-dist').getAbsoluteFSPath();
 const dot = require('dot');
 
 const load = f => fs.readFileSync(f).toString();
-const swaggerbase = f => path.join(swaggerUiAssetPath, f);
+
 const source = load(path.join(__dirname, './template.dot'));
 
-dot.templateSettings = {
-  evaluate:    /\{\{([\s\S]+?)\}\}/g,
-  interpolate: /\{\{=([\s\S]+?)\}\}/g,
-  encode:      /\{\{!([\s\S]+?)\}\}/g,
-  use:         /\{\{#([\s\S]+?)\}\}/g,
-  define:      /\{\{##\s*([\w\.$]+)\s*(\:|=)([\s\S]+?)#\}\}/g,
-  conditional: /\{\{\?(\?)?\s*([\s\S]*?)\s*\}\}/g,
-  iterate:     /\{\{~\s*(?:\}\}|([\s\S]+?)\s*\:\s*([\w$]+)\s*(?:\:\s*([\w$]+))?\s*\}\})/g,
-  varname: 'it',
-  strip: false,
-  append: true,
-  selfcontained: false
-};
-
-const maybeStringify = spec => typeof spec === 'object' ? JSON.stringify(spec) : spec;
+const swaggerload = f => load(path.join(swaggerUiAssetPath, f));
 const throwError = () => { throw new Error('spec was not defined!'); }
+const maybeStringify = spec => typeof spec === 'object' ? JSON.stringify(spec) : spec;
 const maybeError = spec => spec || throwError();
+
+dot.templateSettings = require('./settings');
 
 module.exports = spec => {
   const data = {
-    swaggerUIBundle: load(swaggerbase('./swagger-ui-bundle.js')),
-    swaggerUIStandalonePreset: load(swaggerbase('./swagger-ui-standalone-preset.js')),
-    swaggerUIStyle: load(swaggerbase('./swagger-ui.css')),
+    swaggerUIBundle: swaggerload('./swagger-ui-bundle.js'),
+    swaggerUIStandalonePreset: swaggerload('./swagger-ui-standalone-preset.js'),
+    swaggerUIStyle: swaggerload('./swagger-ui.css'),
   };
   return dot.template(source)(Object.assign({}, data, { spec: maybeStringify(maybeError(spec)) }));
 };
