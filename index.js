faf9ed76011c5032a5815b4d4c09f4e00a669cc7
Rob <rob_zoszak@yahoo.com.au>
2017-11-22 19:43:57 +0800

    adds better validation

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ const source = load(path.join(__dirname, './template.dot'));
 
 const swaggerload = f => load(path.join(swaggerUiAssetPath, f));
 const throwError = () => { throw new Error('spec was not defined!'); }
-const maybeStringify = spec => typeof spec === 'object' ? JSON.stringify(spec) : spec;
-const maybeError = spec => spec || throwError();
+const maybeError = spec => (typeof spec === 'object' && spec) || throwError();
 
 dot.templateSettings = require('./settings');
 
 module.exports = spec => {
   const data = {
-    swaggerUIBundle: swaggerload('./swagger-ui-bundle.js'),
-    swaggerUIStandalonePreset: swaggerload('./swagger-ui-standalone-preset.js'),
-    swaggerUIStyle: swaggerload('./swagger-ui.css'),
+    swaggerUIBundle: swaggerload('swagger-ui-bundle.js'),
+    swaggerUIStandalonePreset: swaggerload('swagger-ui-standalone-preset.js'),
+    swaggerUIStyle: swaggerload('swagger-ui.css'),
   };
-  return dot.template(source)(Object.assign({}, data, { spec: maybeStringify(maybeError(spec)) }));
+  return dot.template(source)(Object.assign({}, data, { spec: JSON.stringify(maybeError(spec)) }));
 };
